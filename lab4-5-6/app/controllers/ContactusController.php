8ad0639dbf7a662e3d786250700a105018ad32ec
asdred <smvberw@gmail.com>
2016-04-18 11:43:25 +0500

    4-5-6 лабы. Фреймворк Phalcon

---
+++ lab4-5-6/app/controllers/ContactusController.php
@@ -0,0 +1,19 @@
+<?php
+
+class ContactusController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        $key = "333555777";
+        
+        // Запись в сессию
+        
+        $this->session->set("token", md5($ip . $agent . $key));
+    }
+
+}
+
---
+++ lab4-5-6/app/models/Report.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Report extends Model
+{
+    public $id;
+
+    public $name;
+
+    public $sname;
+    
+    public $email;
+    
+    public $website;
+    
+    public $meesage;
+}
